f50878b2b87c1987e4f9b24566be0a166325ae01
Andre Dobroskok <>
2026-06-25 14:57:18 +1000

    PROD-737 correct metadta namespaces

--- src/esaml.erl
+++ src/esaml.erl
@@ -595,7 +595,7 @@ to_xml(#esaml_logoutresp{version = V, issue_instant  = Time,
           attributes = [
               #xmlAttribute{name = 'xmlns:md', value = atom_to_list(proplists:get_value("md", Ns#xmlNamespace.nodes))},
               #xmlAttribute{name = 'xmlns:saml', value = atom_to_list(proplists:get_value("saml", Ns#xmlNamespace.nodes))},
-              #xmlAttribute{name = 'xmlns:dsig', value = atom_to_list(proplists:get_value("dsig", Ns#xmlNamespace.nodes))},
+              #xmlAttribute{name = 'xmlns:ds', value = atom_to_list(proplists:get_value("dsig", Ns#xmlNamespace.nodes))},
               #xmlAttribute{name = 'entityID', value = EntityID}
           ], content = [
               SPSSODescriptorElem,
